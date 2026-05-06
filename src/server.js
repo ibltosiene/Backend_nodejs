00bb7e82c1b29837b965c585352f80377c1af3be
ibltosiene <nguyentronglinh180805@gmail.com>
2026-05-06 09:22:27 +0700

    mainline

---
+++ src/server.js
@@ -0,0 +1,18 @@
+const express = require('express')
+require('dotenv').config()
+const configViewEngine = require('./config/viewEngine');
+const webRoutes = require('./routes/web')
+
+const app = express()
+const port = process.env.PORT || 8081
+const hostname = process.env.HOST_NAME || 'localhost'
+
+//config template engine
+configViewEngine(app);
+
+//khai bao route
+app.use('/', webRoutes)
+
+app.listen(port, hostname, () => {
+    console.log(`Example app listening on port ${port}`)
+})
